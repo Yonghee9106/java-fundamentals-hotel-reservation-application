8131b39e1df7443a90fee8bfe6f18a5ccd2e44d9
Yonghee9106 <study910626@gmail.com>
2023-12-10 14:12:17 +1100

    create the room class

---
+++ src/model/Room.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Room implements IRoom {
+    private final String roomNumber;
+    private final Double price;
+    private final RoomType enumeration;
+
+    public Room(String roomNumber, Double price, RoomType enumeration) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.enumeration = enumeration;
+    }
+
+    @Override
+    public String getRoomNumber() {
+        return null;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return null;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return null;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Room Number: " + roomNumber
+                + ", Price: " + price
+                + ", Type: " + enumeration;
+    }
+}
