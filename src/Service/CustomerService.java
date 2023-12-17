2b9ff61641c19d1b6372d6337c5e65a7bfab543f
Yonghee9106 <study910626@gmail.com>
2023-12-17 23:13:35 +1100

    new method

--- src/Service/CustomerService.java
+++ src/Service/CustomerService.java
@@ -1,7 +1,19 @@
 package Service;
 
+import model.Customer;
+
+import java.util.Collection;
+
 public class CustomerService {
     public void addCustomer(String email, String firstName, String lastName) {
 
+    }
+
+    public Customer getCustomer(String customerEmail){
+        return null;
+    }
+
+    public Collection<Customer> getAllCustomer(){
+        return null;
     }
 }
