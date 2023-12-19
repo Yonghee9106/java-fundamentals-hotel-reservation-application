6b6c05376f0ec37111e6440c6736de5373432d46
Yonghee9106 <study910626@gmail.com>
2023-12-19 17:37:15 +1100

    addRoom getARoom method

--- src/Service/ReservationService.java
+++ src/Service/ReservationService.java
@@ -1,4 +1,14 @@
 package Service;
 
+import model.IRoom;
+
 public class ReservationService {
+
+    public void addRoom(IRoom room) {
+
+    }
+
+    public IRoom getARoom(String roomId) {
+        return null;
+    }
 }
