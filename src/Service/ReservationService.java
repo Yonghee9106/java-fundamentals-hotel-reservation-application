2c93741f3285e012d5960816faf6e670abc66166
Yonghee9106 <study910626@gmail.com>
2023-12-20 20:18:50 +1100

    new method

--- src/Service/ReservationService.java
+++ src/Service/ReservationService.java
@@ -1,6 +1,11 @@
 package Service;
 
+import model.Customer;
 import model.IRoom;
+import model.Reservation;
+
+import java.util.Collection;
+import java.util.Date;
 
 public class ReservationService {
 
@@ -9,6 +14,14 @@ public class ReservationService {
     }
 
     public IRoom getARoom(String roomId) {
+        return null;
+    }
+
+    public Reservation reserveARoom(Customer custoemr, IRoom room, Date checkInData, Date checkOutDate) {
+        return null;
+    }
+
+    public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) {
         return null;
     }
 }
