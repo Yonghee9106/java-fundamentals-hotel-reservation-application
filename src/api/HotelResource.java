6af89f5fb89548a04d78090e932586c159f71444
Yonghee9106 <study910626@gmail.com>
2023-12-25 22:36:07 +1100

    new methods

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,4 +1,13 @@
 package api;
 
+import model.Customer;
+
 public class HotelResource {
+    public Customer getCustomer(String email) {
+        return null;
+    }
+
+    public void createACustomer(String eamil, String firstName, String lastName) {
+
+    }
 }
