0f3b1db9327e6b8cee0e7d4c68b0b60838b77f41
Yonghee9106 <study910626@gmail.com>
2023-12-27 15:27:18 +1100

    HotelResource method

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,13 +1,34 @@
 package api;
 
 import model.Customer;
+import model.IRoom;
+import model.Reservation;
+
+import java.util.Collection;
+import java.util.Date;
 
 public class HotelResource {
     public Customer getCustomer(String email) {
         return null;
     }
 
-    public void createACustomer(String eamil, String firstName, String lastName) {
+    public void createACustomer(String email, String firstName, String lastName) {
 
+    }
+
+    public IRoom etRoom(String roomNumber) {
+        return null;
+    }
+
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date CheckOutDate) {
+        return null;
+    }
+
+    public Collection<Reservation> getCustomerReservation(String customerEmail) {
+        return null;
+    }
+
+    public Collection<IRoom> findARoom(Date checkIn, Date checkOut) {
+        return null;
     }
 }
