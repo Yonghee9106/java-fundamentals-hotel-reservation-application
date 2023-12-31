54473031e419f1315dbf4942ac7459634c29a202
Yonghee9106 <study910626@gmail.com>
2024-01-01 10:32:54 +1100

    menu print

--- src/Driver.java
+++ src/Driver.java
@@ -2,6 +2,14 @@ import model.Customer;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("\nWelcome to the Hotel Reservation Application\n" +
+                "--------------------------------------------\n" +
+                "1. Find and reserve a room\n" +
+                "2. See my reservations\n" +
+                "3. Create an Account\n" +
+                "4. Admin\n" +
+                "5. Exit\n" +
+                "--------------------------------------------\n" +
+                "Please select a number for the menu option:\n");
     }
 }
