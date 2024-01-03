93a9eb02ba63757ad5a0ce2b8dfc3317c37c0463
Yonghee9106 <study910626@gmail.com>
2024-01-03 23:23:24 +1100

    display main menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,5 +1,17 @@
 public class MainMenu {
     public static void mainMenu() {
+        displayMainMenu();
+    }
 
+    private static void displayMainMenu() {
+        System.out.println("\nWelcome to the Hotel Reservation Application\n\n" +
+                "-------------------------------------------------------\n" +
+                "1. Find and reserve a room\n" +
+                "2. See my reservations\n" +
+                "3. Create an Account\n" +
+                "4. Admin\n" +
+                "5. Exit\n" +
+                "-------------------------------------------------------\n" +
+                "Please select a number for the menu option\n");
     }
 }
