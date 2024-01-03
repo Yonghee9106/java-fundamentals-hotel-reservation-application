5924fcc4bfb32d53f6ff9b9b1d6ef9c8f5aae6bd
Yonghee9106 <study910626@gmail.com>
2024-01-03 23:59:07 +1100

    choose option

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -6,7 +6,39 @@ public class MainMenu {
         Scanner scanner = new Scanner(System.in);
 
         displayMainMenu();
-        selectedMenu = scanner.nextLine();
+
+        try {
+            do {
+                selectedMenu = scanner.nextLine();
+
+                if (selectedMenu.length() == 1) {
+                    switch (selectedMenu.charAt(0)) {
+                        case '1':
+                            System.out.println("1");
+                            break;
+                        case '2':
+                            System.out.println("2");
+                            break;
+                        case '3':
+                            System.out.println("3");
+                            break;
+                        case '4':
+                            System.out.println("4");
+                            break;
+                        case '5':
+                            System.out.println("Exit");
+                            break;
+                        default:
+                            System.out.println("Please enter a valid option number (1-5)\n");
+                            break;
+                    }
+                } else {
+                    System.out.println("Please enter a single-digit option number (1-5)\n");
+                }
+            } while (selectedMenu.charAt(0) != '5' || selectedMenu.length() != 1);
+        } catch (Exception ex) {
+            System.err.println("An error occurred: " + ex.getMessage());
+        }
     }
 
     private static void displayMainMenu() {
